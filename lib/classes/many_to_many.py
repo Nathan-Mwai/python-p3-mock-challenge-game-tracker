9bd14793319ab56bdff999662ccce343d401b486
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-12 14:44:48 +0300

    finishes lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -2,85 +2,90 @@ class Game:
     def __init__(self, title):
         self.title = title
         self._results = []
+
     @property
     def title(self):
         return self._title
-    
+
     @title.setter
     def title(self, value):
         if not isinstance(value, str):
             raise Exception('Title must be in a string format')
-        
         if hasattr(self, '_title') and self._title is not None:
             raise Exception('Cannot change title once made')
-        
-        if not len(value) > 0:
+        if not value:
             raise Exception('Title must be written')
         self._title = value
-    
+
     def add_result(self, value):
         if not isinstance(value, Result):
-            raise Exception('result mus be an instance of Result')
+            raise Exception('Result must be an instance of Result')
         self._results.append(value)
-    
+
     def results(self):
         return self._results
 
     def players(self):
         players_done = set()
-        
         for result in self._results:
             if isinstance(result.player, Player):
                 players_done.add(result.player)
-                
         return list(players_done)
 
     def average_score(self, player):
-        pass
+        if not isinstance(player, Player):
+            raise Exception('Player doesn\'t exist')
+
+        scores = [result.score for result in self._results if result.player == player]
+        
+        if not scores:
+            return None
+        
+        return sum(scores) / len(scores)
+
 
 class Player:
     def __init__(self, username):
         self.username = username
         self._results = []
-        
+
     @property
-    #  I'm using property to check if certain criteria are met
     def username(self):
         return self._username
-    
+
     @username.setter
-    # filling in criteria to be met
     def username(self, value):
         if not isinstance(value, str):
             raise Exception('Name must be in string format')
-        
         if not (2 <= len(value) <= 16):
             raise Exception('Name must be between 2 and 16 characters')
-        
         self._username = value
-        
+
     def add_results(self, result):
         if not isinstance(result, Result):
-            raise Exception('results must be an instance of Result')
+            raise Exception('Result must be an instance of Result')
         self._results.append(result)
-        
+
     def results(self):
         return self._results
 
     def games_played(self):
-        
         games_done = set()
-        
         for result in self._results:
             if isinstance(result.game, Game):
                 games_done.add(result.game)
         return list(games_done)
 
     def played_game(self, game):
-        pass
+        if not isinstance(game, Game):
+            raise Exception('Game doesn\'t exist')
+        return any(result.game == game for result in self._results)
 
     def num_times_played(self, game):
-        pass
+        if not isinstance(game, Game):
+            raise Exception('Game doesn\'t exist')
+        return sum(1 for result in self._results if result.game == game)
+
 
 class Result:
     
