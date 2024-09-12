225d22a625f629678324f66e3b25f3f9499dc8f2
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-12 13:14:52 +0300

    FInished ORM for player

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,7 +1,7 @@
 class Game:
     def __init__(self, title):
         self.title = title
-    
+        self._results = []
     @property
     def title(self):
         return self._title
@@ -17,9 +17,14 @@ class Game:
         if not len(value) > 0:
             raise Exception('Title must be written')
         self._title = value
-        
+    
+    def add_results(self, value):
+        if not isinstance(value, Result):
+            raise Exception('result mus be an instance of Result')
+        self._results.append(value)
+    
     def results(self):
-        pass
+        return self._results
 
     def players(self):
         pass
@@ -30,6 +35,7 @@ class Game:
 class Player:
     def __init__(self, username):
         self.username = username
+        self._results = []
         
     @property
     #  I'm using property to check if certain criteria are met
@@ -47,11 +53,20 @@ class Player:
         
         self._username = value
         
+    def add_results(self, result):
+        if not isinstance(result, Result):
+            raise Exception('results must be an instance of Result')
+        self._results.append(result)
+        
     def results(self):
-        pass
+        return self._results
 
     def games_played(self):
-        pass
+        games_done = set()
+        for games in self._results:
+            if isinstance(games.player, Game):
+                games_done.add(games.player)
+        return list(games_done)
 
     def played_game(self, game):
         pass
@@ -60,11 +75,18 @@ class Player:
         pass
 
 class Result:
+    
+    all = []
+    
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
-    
+        
+        Result.all.append(self)
+        player.add_results(self)
+        game.add_results(self)
+        
     @property
     def score(self):
         return self._score
@@ -80,3 +102,23 @@ class Result:
         if hasattr(self, '_score') and self._score is not None:
             raise Exception('The score cannot be changed')
         self._score = amount
+    
+    @property
+    def player(self):
+        return self._player
+    
+    @player.setter
+    def player(self, value):
+        if not isinstance(value, Player):
+            raise Exception('player must be a Player')
+        self._player = value
+    
+    @property
+    def game(self):
+        return self._game
+    
+    @game.setter
+    def game(self, value):
+        if not isinstance(value, Game):
+            raise Exception('game must be part of Game')
+        self._player = value
