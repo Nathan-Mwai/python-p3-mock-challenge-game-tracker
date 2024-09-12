f3c512a434287aa500debacbf629a591dcb6914c
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-12 12:04:28 +0300

    finishes player initializer and properties

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -14,8 +14,8 @@ class Game:
         if hasattr(self, '_title') and self._title is not None:
             raise Exception('Cannot change title once made')
         
-        if not (2 <= len(value) <= 16):
-            raise Exception('Title must be between 2 and 16 characters')
+        if not len(value) > 0:
+            raise Exception('Title must be written')
         self._title = value
         
     def results(self):
@@ -30,7 +30,23 @@ class Game:
 class Player:
     def __init__(self, username):
         self.username = username
-
+        
+    @property
+    #  I'm using property to check if certain criteria are met
+    def username(self):
+        return self._username
+    
+    @username.setter
+    # filling in criteria to be met
+    def username(self, value):
+        if not isinstance(value, str):
+            raise Exception('Name must be in string format')
+        
+        if not (2 <= len(value) <= 16):
+            raise Exception('Name must be between 2 and 16 characters')
+        
+        self._username = value
+        
     def results(self):
         pass
 
